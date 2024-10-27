054f6c8e7120fadda983a31b7056578e466470ce
Mans <mariaansantana2004@gmail.com>
2024-10-27 04:08:15 -0300

    added user model

---
+++ backend/app/models/user.py
@@ -0,0 +1,13 @@
+from app import db
+
+class User(db.Model):
+    __tablename__ = 'tb_ppf_user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+def __repr__(self):
+        return f'<User {self.name}>'
