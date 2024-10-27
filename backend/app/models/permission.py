58be7ef73c935d07f983d3f0691dc73060c3c017
Mans <mariaansantana2004@gmail.com>
2024-10-27 04:16:47 -0300

    added permission model

---
+++ backend/app/models/permission.py
@@ -0,0 +1,17 @@
+from app import db
+
+class Permission(db.Model):
+    __tablename__ = 'tb_ppf_permission'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    folder_id = db.Column(db.Integer, db.ForeignKey('folders.id'), nullable=False)
+
+    user = db.relationship('User', back_populates='permissions')
+    folder = db.relationship('Folder')
+
+    id = db.Column(db.Integer, primary_key = True)
+
+    def __repr__(self):
+        return f'<Permission User {self.user_id} Folder {self.folder_id}>'
+    
