4a1d161f96de533cf7974fc301944bac80b47f22
Mans <offonmaria@hotmail.com>
2024-12-02 20:12:07 -0300

    Libs Upgrade

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:34083554@localhost:3306/paperflux_dev'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:raphaelcabecadeovo@localhost:3306/paperflux_dev'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
